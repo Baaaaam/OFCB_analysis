9f6e02433e348a6e666f033d2bdcb47293ed505a
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-02-29 22:09:45 -0600

    some change

--- T1/analys.py
+++ T1/analys.py
@@ -7,65 +7,6 @@ import matplotlib.patches as mpatches
 mpl.rc('font', family='serif', size=11)
 mpl.rc('savefig', bbox='tight')
 
-import george
-import george.kernels
-from george.kernels import ExpSquaredKernel
-import scipy.optimize as op
-
-
-# Set up the Gaussian process.
-def init_gp(t, y, yerr):
-  y_mean = np.mean(y)
-    kernel = float(y_mean) * ExpSquaredKernel(1.0)
-    gp = george.GP(kernel, mean=y_mean)
-    gp.compute(t, yerr=yerr, sort=False)
-    return gp
-
-def negloglikelihood(gp, y, hyperparams):
-  gp.kernel[:] = hyperparams
-    ll = gp.lnlikelihood(y, quiet=True)
-    return -ll if np.isfinite(ll) else 1e25  # mask infinites
-
-def grad_negloglikelihood(gp, y, hyperparams):
-  gp.kernel[:] = hyperparams
-    gll = gp.grad_lnlikelihood(y, quiet=True)
-    #gll[~np.isfinite(gll)] = 0.0
-    return -gll
-
-def optimize_hyperparams(gp, y):
-  hp0 = gp.kernel.vector
-    # form some closures
-    nll = lambda hp: negloglikelihood(gp, y, hp)
-    grad_nll = lambda hp: grad_negloglikelihood(gp, y, hp)
-    results = op.minimize(nll, hp0, jac=grad_nll)
-    gp.kernel[:] = results.x
-
-def model(t, y, tstar, yerr=1e-6):
-  gp = init_gp(t, y, yerr)
-    optimize_hyperparams(gp, y)
-    mu, cov = gp.predict(y, tstar)
-    std = np.sqrt(np.diag(cov))
-    return mu, std, gp
-
-
-def model_plot(t, y, yerr, tpred, mu, std, title=None, ymax=None, show_title=True):
-  fig = plt.figure(figsize=(8, 8))
-    T = len(t) / 2
-    plt.errorbar(t[:T], y[:T], yerr if yerr is None else yerr[:T], fmt='r.', label='DYMOND')
-    plt.errorbar(t[T:], y[T:], yerr if yerr is None else yerr[T:], fmt='rx', label='Cyclus')
-    plt.plot(tpred, mu, 'k-', label='model')
-    plt.fill_between(tpred, mu - 2*std, mu + 2*std, color='gray')
-    ax = plt.axis()
-    plt.axis([tpred[0], tpred[-1], 0.0, ymax or ax[3]])
-    plt.xticks(np.linspace(tpred[0], tpred[-1], 5))
-    plt.legend(loc=0)
-    plt.xlabel('Time [year]')
-    plt.ylabel('Generated Power [GWe]')
-    if title and show_title:
-      plt.title(title)
-    fbase = 'gwe-model-' + (title or '').lower().replace(' ', '-')
-    plt.savefig(fbase + '.eps')
-    plt.savefig(fbase + '.png')
 
 
 
@@ -167,7 +108,7 @@ def main():
   print(len(class_in[:-1]))
   print(len(cyclus_in))
 
-  run_dtw(cyclus_time, class_in[:-1], cyclus_in)
+  run_dtw(cyclus_time, class_out[:-1], cyclus_out)
 
 
 main()
